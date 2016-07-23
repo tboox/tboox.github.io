3152575a42994319501b97981e354e869c6e6529
ruki <waruqi@gmail.com>
2016-07-23 08:56:12 +0800

    update links

--- js/books.js
+++ js/books.js
@@ -13,43 +13,43 @@
      * 内容JSON
      */
     var content = [{
-        page_link: 'https://www.amazon.com/gp/product/0131103628?ie=UTF8&tag=tboox-20&camp=1789&linkCode=xm2&creativeASIN=0131103628',
+        page_link: 'https://www.amazon.com/gp/product/0131103628/ref=as_li_tl?ie=UTF8&tag=tboox-20&camp=1789&creative=9325&linkCode=as2&creativeASIN=0131103628&linkId=1014f2036ed02046027425e274cd704f',
         img_link: '/static/img/books/en/c-programing.jpg',
         title: 'The C Programming Language (2nd Edition)',
         description: 'The authors present the complete guide to ANSI standard C language programming'
     },
     {
-        page_link: 'https://www.amazon.com/gp/product/B011DC1XRW?ie=UTF8&tag=tboox-20&camp=1789&linkCode=xm2&creativeASIN=B011DC1XRW',
+        page_link: 'https://www.amazon.com/gp/product/111860864X/ref=as_li_tl?ie=UTF8&tag=tboox-20&camp=1789&creative=9325&linkCode=as2&creativeASIN=111860864X&linkId=d33c3c92c278171d902281baf5799838',
         img_link: '/static/img/books/en/android-hacker.jpg',
         title: 'Android Hacker\'s Handbook (1st Edition)',
         description: 'A complete guide to securing the Android operating system'
     },
     {
-        page_link: 'https://www.amazon.com/gp/product/1568814240?ie=UTF8&tag=tboox-20&camp=1789&linkCode=xm2&creativeASIN=1568814240',
+        page_link: 'https://www.amazon.com/gp/product/1568814240/ref=as_li_tl?ie=UTF8&tag=tboox-20&camp=1789&creative=9325&linkCode=as2&creativeASIN=1568814240&linkId=f0c94c1ea163a20f09699d27fea9b63e',
         img_link: '/static/img/books/en/real-time-render.jpg',
         title: 'Real-Time Rendering (3rd Edition)',
         description: 'Thoroughly revised, this third edition focuses on modern techniques used to generate synthetic three-dimensional images in a fraction of a second'
     },
     {
-        page_link: 'https://www.amazon.com/gp/product/1118057651?ie=UTF8&tag=tboox-20&camp=1789&linkCode=xm2&creativeASIN=1118057651',
+        page_link: 'https://www.amazon.com/gp/product/1118057651/ref=as_li_tl?ie=UTF8&tag=tboox-20&camp=1789&creative=9325&linkCode=as2&creativeASIN=1118057651&linkId=2b0b389bcc3b221ce4a5b4c3eff7d70b',
         img_link: '/static/img/books/en/macosx-ios-internals.jpg',
         title: 'Mac OS X and iOS Internals: To the Apple\'s Core (1st Edition)',
         description: 'From architecture to implementation, this book is essential reading if you want to get serious about the internal workings of Mac OS X and iOS'
     },
     {
-        page_link: 'https://www.amazon.com/gp/product/0132017997?ie=UTF8&tag=tboox-20&camp=1789&linkCode=xm2&creativeASIN=0132017997',
+        page_link: 'https://www.amazon.com/gp/product/0132017997/ref=as_li_tl?ie=UTF8&tag=tboox-20&camp=1789&creative=9325&linkCode=as2&creativeASIN=0132017997&linkId=aacd921f26657abe01e34423875bf0a8',
         img_link: '/static/img/books/en/unix-os.jpg',
         title: 'The Design of the UNIX Operating System (1st Edition)',
         description: 'This book describes the internal algorithms and the structures that form the basis of the UNIX operating system and their relationship to the programmer interface'
     },
     {
-        page_link: 'https://www.amazon.com/gp/product/0735619670?ie=UTF8&tag=tboox-20&camp=1789&linkCode=xm2&creativeASIN=0735619670',
+        page_link: 'https://www.amazon.com/gp/product/0735619670/ref=as_li_tl?ie=UTF8&tag=tboox-20&camp=1789&creative=9325&linkCode=as2&creativeASIN=0735619670&linkId=40e6b2dd60c583ba4a820fee08602c3d',
         img_link: '/static/img/books/en/code-complete.jpg',
         title: 'Code Complete: A Practical Handbook of Software Construction (2nd Edition)',
         description: 'Widely considered one of the best practical guides to programming'
     },
     {
-        page_link: 'https://www.amazon.com/gp/product/1118787315?ie=UTF8&tag=tboox-20&camp=1789&linkCode=xm2&creativeASIN=1118787315',
+        page_link: 'https://www.amazon.com/gp/product/1118787315/ref=as_li_tl?ie=UTF8&tag=tboox-20&camp=1789&creative=9325&linkCode=as2&creativeASIN=1118787315&linkId=b26e34cd334d36328b2a80c9a5d4adc1',
         img_link: '/static/img/books/en/reverse-engineering.jpg',
         title: 'Practical Reverse Engineering: x86, x64, ARM, Windows Kernel, Reversing Tools, and Obfuscation (1st Edition)',
         description: 'The book covers x86, x64, and ARM (the first book to cover all three); Windows kernel-mode code rootkits and drivers; virtual machine protection techniques; and much more'
