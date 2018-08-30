149c08f805c0ba64dbd343692460a8eff705961d
ruki <waruqi@gmail.com>
2018-08-30 13:41:42 +0800

    add books

--- js/books.cn.js
+++ js/books.cn.js
@@ -13,61 +13,61 @@
      * 内容JSON
      */
     var content = [{
-        page_link: 'http://www.amazon.cn/gp/product/B01FE26HAU/ref=as_li_tf_tl?ie=UTF8&camp=536&creative=3200&creativeASIN=B01FE26HAU&linkCode=as2&tag=tboox01-23',
+        page_link: 'https://amazon.cn/gp/product/B01FE26HAU/ref=as_li_tl?ie=UTF8&tag=tboox01-23&camp=536&creative=3200&linkCode=as2&creativeASIN=B01FE26HAU&linkId=1c6195064fc016bac01b77ca9f762ea1',
         img_link: '/static/img/books/cn/c-primer-plus.jpg',
         title: 'C Primer Plus(第6版)(中文版)',
         description: '经久不衰的C语言畅销经典教程，针对C11标准进行全面更新'
     },
     {
-        page_link: 'https://www.amazon.cn/gp/product/B01HI4WMEC/ref=as_li_tf_tl?ie=UTF8&camp=536&creative=3200&creativeASIN=B01HI4WMEC&linkCode=as2&tag=tboox01-23',
-        img_link: '/static/img/books/cn/password-tech.jpg',
-        title: '图解密码技术(第3版)',
-        description: '以图配文的形式，详细讲解了6种最重要的密码技术：对称密码、公钥密码、单向散列函数、消息认证码、数字签名和伪随机数生成器'
+        page_link: 'https://amazon.cn/gp/product/B008A4XZRI/ref=as_li_tl?ie=UTF8&tag=tboox01-23&camp=536&creative=3200&linkCode=as2&creativeASIN=B008A4XZRI&linkId=5b971841b71dda197d2717e10e0ced9f',
+        img_link: '/static/img/books/cn/cplusplus_primer_plus.jpg',
+        title: 'C++ Primer Plus中文版(第6版)',
+        description: '一本经久不衰的C++畅销经典教程；一本支持C++11新标准的程序设计图书'
     },
     {
-        page_link: 'http://www.amazon.cn/gp/product/B01H1RZTIM/ref=as_li_tf_tl?ie=UTF8&camp=536&creative=3200&creativeASIN=B01H1RZTIM&linkCode=as2&tag=tboox01-23',
-        img_link: '/static/img/books/cn/linux-programing.jpg',
-        title: 'Linux环境编程:从应用到内核',
-        description: '从一个全新的角度带领读者重新进入Linux环境编程，从应用出发，深入内核源码，研究Linux各接口的工作机制和原理'
+        page_link: 'https://amazon.cn/gp/product/B073LWHBBY/ref=as_li_tl?ie=UTF8&tag=tboox01-23&camp=536&creative=3200&linkCode=as2&creativeASIN=B073LWHBBY&linkId=c8edf83e2b4f28bd29fe27c84d9ce77f',
+        img_link: '/static/img/books/cn/deep_learning.jpg',
+        title: '深度学习(deep learning)',
+        description: '由全球知名的三位专家Ian Goodfellow、Yoshua Bengio 和Aaron Courville撰写，是深度学习领域奠基性的经典教材'
     },
     {
-        page_link: 'http://www.amazon.cn/gp/product/B00513FBZK/ref=as_li_tf_tl?ie=UTF8&camp=536&creative=3200&creativeASIN=B00513FBZK&linkCode=as2&tag=tboox01-23',
+        page_link: 'https://amazon.cn/gp/product/B071436YV8/ref=as_li_tl?ie=UTF8&tag=tboox01-23&camp=536&creative=3200&linkCode=as2&creativeASIN=B071436YV8&linkId=f0304f2b625edcca430123e1d1a3e219',
         img_link: '/static/img/books/cn/digital-image.jpg',
-        title: '数字图像处理(第3版)',
+        title: '数字图像处理(第三版)',
         description: '数字图像处理领域的经典之作'
     },
     {
-        page_link: 'https://www.amazon.cn/gp/product/B0039K94IC/ref=as_li_tf_tl?ie=UTF8&camp=536&creative=3200&creativeASIN=B0039K94IC&linkCode=as2&tag=tboox01-23',
-        img_link: '/static/img/books/cn/pattern-recognition.jpg',
-        title: '模式识别(第4版)',
-        description: '全面阐述了模式识别的基础理论、最新方法以及各种应用。模式识别是信息科学和人工智能的重要组成部分，主要应用领域有图像分析、光学字符识别、信道均衡、语言识别和音频分类等'
+        page_link: 'https://amazon.cn/gp/product/B06XKCV7X9/ref=as_li_tl?ie=UTF8&tag=tboox01-23&camp=536&creative=3200&linkCode=as2&creativeASIN=B06XKCV7X9&linkId=c4e0c902942c95a0b53ee37ab8a85e6f',
+        img_link: '/static/img/books/cn/image_algorithom.jpg',
+        title: '算法图解',
+        description: '图文并茂，以让人容易理解的方式阐释了算法，旨在帮助程序员在日常项目中更好地发挥算法的能量'
     },
     {
-        page_link: 'http://www.amazon.cn/gp/product/B00DMS9990/ref=as_li_tf_tl?ie=UTF8&camp=536&creative=3200&creativeASIN=B00DMS9990&linkCode=as2&tag=tboox01-23',
-        img_link: '/static/img/books/cn/image-tcp-ip.jpg',
-        title: '图灵程序设计丛书:图解TCP/IP(第5版)',
-        description: '这是一本图文并茂的网络管理技术书籍，旨在让广大读者理解TCP/IP的基本知识、掌握TCP/IP的基本技能'
+        page_link: 'https://amazon.cn/gp/product/B00AK7BYJY/ref=as_li_tl?ie=UTF8&tag=tboox01-23&camp=536&creative=3200&linkCode=as2&creativeASIN=B00AK7BYJY&linkId=99c547b8017e99ccd179cc2c6f7b9835',
+        img_link: '/static/img/books/cn/algorithom.jpg',
+        title: '算法导论',
+        description: '全球超过50万人阅读的算法圣经！算法标准教材，国内外1000余所高校采用。国内知名高校6位教授历时3年倾心翻译'
     },
     {
-        page_link: 'http://www.amazon.cn/gp/product/B011S72JB6/ref=as_li_tf_tl?ie=UTF8&camp=536&creative=3200&creativeASIN=B011S72JB6&linkCode=as2&tag=tboox01-23',
-        img_link: '/static/img/books/cn/unix-network.jpg',
-        title: 'UNIX网络编程(卷1):套接字联网API(第3版)',
-        description: '这是一部传世之作！书中吸纳了近几年网络技术的发展，增添了IPv6、SCTP协议和密钥管理套接字等内容，深入讨论了最新的关键标准、实现和技术'
+        page_link: 'https://amazon.cn/gp/product/B07G1YNCRC/ref=as_li_tl?ie=UTF8&tag=tboox01-23&camp=536&creative=3200&linkCode=as2&creativeASIN=B07G1YNCRC&linkId=2f30f551010881b8f3e27ea2735d5c42',
+        img_link: '/static/img/books/cn/encrypt_and_decrypt.jpg',
+        title: '加密与解密',
+        description: '由看雪软件安全论众多高手共同打造而成，讲述了软件安全领域许多基础知识和技能，如调试技能、逆向分析、加密保护、外壳开发、虚拟机设计等'
     },
     {
-        page_link: 'http://www.amazon.cn/gp/product/B0061XKRXA/ref=as_li_tf_tl?ie=UTF8&camp=536&creative=3200&creativeASIN=B0061XKRXA&linkCode=as2&tag=tboox01-23',
-        img_link: '/static/img/books/cn/code-handbook.jpg',
-        title: '代码大全(第2版)',
-        description: '这是一本百科全书式的软件构建手册，涵盖了软件构建活动的方方面面，尤其强调提高软件质量的种种实践方法'
+        page_link: 'https://amazon.cn/gp/product/B00163LU68/ref=as_li_tl?ie=UTF8&tag=tboox01-23&camp=536&creative=3200&linkCode=as2&creativeASIN=B00163LU68&linkId=eea1436de0113f9bdcfb659e10fad306',
+        img_link: '/static/img/books/cn/c_and_pointer.jpg',
+        title: 'C和指针 Pointers On C',
+        description: '提供与C语言编程相关的全面资源和深入讨论，本书通过对指针的基础知识和高级特性的探讨，帮助程序员把指针的强大功能融入到自己的程序中去'
     },
     {
-        page_link: 'http://www.amazon.cn/gp/product/B00VHFSR34/ref=as_li_tf_tl?ie=UTF8&camp=536&creative=3200&creativeASIN=B00VHFSR34&linkCode=as2&tag=tboox01-23',
-        img_link: '/static/img/books/cn/android-safe.jpg',
-        title: 'Android安全攻防权威指南',
-        description: '书中细致地介绍了Android系统中的漏洞挖掘、分析，并给出了大量利用工具，结合实例从白帽子角度分析了诸多系统问题，是一本难得的安全指南'
+        page_link: 'https://amazon.cn/gp/product/B00QKU9B7M/ref=as_li_tl?ie=UTF8&tag=tboox01-23&camp=536&creative=3200&linkCode=as2&creativeASIN=B00QKU9B7M&linkId=9e4aafa8fc07ded4272073bde39466ba',
+        img_link: '/static/img/books/cn/graphic.jpg',
+        title: '计算机图形学(第四版)',
+        description: '图形学经典著作'
     },
     {
-        page_link: 'http://www.amazon.cn/gp/product/B008Z1IEQ8/ref=as_li_tf_tl?ie=UTF8&camp=536&creative=3200&creativeASIN=B008Z1IEQ8&linkCode=as2&tag=tboox01-23',
+        page_link: 'https://amazon.cn/gp/product/B008Z1IEQ8/ref=as_li_tl?ie=UTF8&tag=tboox01-23&camp=536&creative=3200&linkCode=as2&creativeASIN=B008Z1IEQ8&linkId=d3445bba1deaaf620b2f19b9a4f3fc41',
         img_link: '/static/img/books/cn/unix-programing-art.jpg',
         title: '传世经典书丛:UNIX编程艺术',
         description: '主要介绍了Unix系统领域中的设计和开发哲学、思想文化体系、原则与经验，由公认的Unix编程大师、开源运动领袖人物之一Eric S.Raymond倾力多年写作而成'
---
+++ page/5books.cn.md
@@ -0,0 +1,20 @@
+---
+layout: book.cn
+title: 书店
+tags: 计算机,图书,IT,书籍,编程开发
+permalink: /book/
+icon: bookmark
+---
+
+
+
+<div class="grid">
+</div>
+
+<script src="{{ " /js/masonry.pkgd.min.js " | prepend: site.baseurl }}" charset="utf-8"></script>
+<script src="{{ " /js/books.cn.js " | prepend: site.baseurl }}" charset="utf-8"></script>
+
+
+{% include comments.html %}
+
+
